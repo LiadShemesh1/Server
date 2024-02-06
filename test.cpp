735ef5553ebda925888da494088b68297f39335f
Liad Shemesh <liad643@gmail.com>
2024-02-06 15:37:58 +0200

    handle connection

--- test.cpp
+++ test.cpp
@@ -3,19 +3,23 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
 #include <stdbool.h>
 #include <limits.h>
+#include <iostream>
+#include <netdb.h>
 
-#define SERVERPORT 8989
+#define SERVERPORT "8989"  // the port users will be connecting to
 #define BUFSIZE 4096
 #define SOCKETERROR (-1)
-#define SERVER_BACKLOG 1
+#define SERVER_BACKLOG 1 // how many pending connections queue will hold
 
 
 // typedef allows us to use these, with a more meaningful name
-typedef struct sockaddr_in SA_IN;
 typedef struct sockaddr SA;
+typedef struct sockaddr_storage SAS; 
+typedef struct addrinfo ADDINFO;
 
 // here for linking (or compiling), actual implimintation is down.
 void handle_connection(int client_socket);
@@ -23,21 +27,111 @@ void handle_connection(int client_socket);
 //seccses and failing (-1). 
 int check(int exp, const char *msg);
 
+void *get_in_addr(struct sockaddr *sa);
+
 int main(int argc , char **argv)
 {
-    int server_socket, client_socket, addr_size;
-    SA_IN server_addr, client_addr;
 
-    // craete the socket: AF_INET = IPv4, SOCK_STREAM = TCP, 0 = unspecified default protocol appropriate for the requested socket type.
-    check((server_socket = socket(AF_INET, SOCK_STREAM, 0)), 
-            "Failed to create socket");
+        int server_socket, client_socket; // client_socket not sure yet
+        socklen_t addr_size;
+        SAS their_addr;
+        ADDINFO hints, *res;
+        char s[INET6_ADDRSTRLEN];  // taking the maximum size of an address
 
-    //initializing the address struct
-    server_addr.sin_family = AF_INET; // IPv4
-    server_addr.sin_addr.s_addr = INADDR_ANY; // use my IPv4 address
-    server_addr.sin_port = htons(SERVERPORT); // Host TO Network Short
-    
-    //understand bind!
-    check((bind(server_socket, (SA*)&server_addr, sizeof(server_addr))), 
-        "Bind Failed");
+        // first, load up address structs with getaddrinfo():
+        memset(&hints, 0, sizeof(hints)); // zero out hints; 
+        //memset ensures that the structure is properly initialized before setting specific fields, 
+        //reducing the chances of encountering bugs related to uninitialized memory.
+        hints.ai_family = AF_INET;       // use IPv4
+        hints.ai_socktype = SOCK_STREAM; // TCP
+        hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+
+        // When successful, getaddrinfo() returns 0 and a pointer to a linked list of one or more
+        // addrinfo structures through the res argument.
+        getaddrinfo(NULL, SERVERPORT, &hints, &res);
+
+        // craete the socket using the *res which points to hints with the parameters above:
+        check((server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol)), 
+                "Failed to create socket");
+        
+        // bind it to the port we passed in to getaddrinfo():
+        check(bind(server_socket, res->ai_addr, res->ai_addrlen), 
+                "Bind Failed");
+        check(listen(server_socket, SERVER_BACKLOG),
+                "Listen Failed");
+
+        freeaddrinfo(res); // free the linked-list
+        
+        while(true)
+        {
+                std::cout << "waiting for connections... \n" << std::endl;
+                addr_size = sizeof(their_addr); // chaeck
+                
+                check(client_socket = 
+                        accept(server_socket, (SA*)&their_addr, &addr_size),
+                        "Accept Faild");
+                std::cout << "Connected \n" << std::endl;
+                
+
+                // print the client address:
+                inet_ntop(their_addr.ss_family,
+                        get_in_addr((SA*)&their_addr),
+                        s, sizeof(s));
+                
+                std::cout << "server: got connection from " << s << '\n' << std::endl;
+
+                
+                // do whatever we want with the connection:
+                handle_connection(client_socket);
+        } // while
+
+        return 0;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(SA *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int check(int exp, const char *msg)
+{
+        if(exp == SOCKETERROR){
+                perror(msg);
+                exit(1);
+        }
+        return exp;
+}
+
+void handle_connection(int client_socket)
+{
+        char buffer[BUFSIZE]; // The size of each buffer we will read
+        size_t bytes_read;
+        int msgsize = 0;
+        char actualpath[PATH_MAX + 1];  // +1 is for the null termination character.
+        
+
+        // read the client's message -- the name of the file to read
+        while((bytes_read = read(client_socket, buffer+msgsize, sizeof(buffer)-msgsize-1)) > 0) {
+                msgsize += bytes_read;
+                if(msgsize > BUFSIZE-1 || buffer[msgsize-1] == '\n') break;
+        }
+        check(bytes_read, "recive error");
+        buffer[msgsize-1] = 0; //null terminate the message and remove the \n
+
+        std::cout << "REQUEST: " << buffer << std::endl;
+        
+        fflush(stdout); // makes sure that all the recived data was written.
+        // fflush(stdout) checks if there are any data in the buffer that should be written and if so,
+        // the underlying syscall is used to write the data to the OS.
+        /*
+        fflush() is typically used for output stream only. Its purpose is to clear (or flush)
+        the output buffer and move the buffered data to console (in case of stdout) 
+        or disk (in case of file output stream)
+        */
 }
